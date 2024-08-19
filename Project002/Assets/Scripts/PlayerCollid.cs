e27b878aba8d5fcae357af9d0ab0cd2cbbeafff1
Wizz <108207747+WizzZn@users.noreply.github.com>
2024-08-19 10:01:17 +0530

    Add Custome Colliders

--- Project002/Assets/Scripts/PlayerCollid.cs
+++ Project002/Assets/Scripts/PlayerCollid.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerCollid : MonoBehaviour
 {
     public int key;
-    public Animator DoorAnim;
-   
+   //public Animator DoorAnim;
 
+    [SerializeField] TextMeshProUGUI keyText;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +27,7 @@ public class PlayerCollid : MonoBehaviour
         {
             other.gameObject.SetActive(false);
             key += 1;
+            keyText.text = key.ToString();
         }
     }
 }
